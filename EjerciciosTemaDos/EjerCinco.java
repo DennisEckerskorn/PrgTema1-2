f72f7b23a5ec48883f3ea3f515438377ff0d8dd2
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-22 15:55:02 +0200

    update

--- EjerciciosTemaDos/EjerCinco.java
+++ EjerciciosTemaDos/EjerCinco.java
@@ -1,11 +1,15 @@
 public class EjerCinco {
 	public static void main(String[]args){
+	//Declaración de las variables
 	int alto = 15;
 	int ancho = 25;
-	double perimetro = 2*(ancho+alto);
-	double area = alto*ancho;
+	//Se calculan las formulas dadas y se guarda el resultado en la variable.
+	int perimetro = 2*(ancho+alto);
+	int area = alto*ancho;
 	
-	System.out.println("El Perímetro del rectangulo es: " + perimetro);
-	System.out.println("El área del rectangulo es: " + area);
+	//Se muestran los resultados por pantalla.
+	System.out.println("El valor dado para el alto del rectangulo son " + alto + "cm " + " y el ancho del rectangulo son " + ancho + "cm "); 
+	System.out.println("El Perímetro del rectangulo es: " + perimetro + "cm");
+	System.out.println("El área del rectangulo es: " + area + "cm");
 	}
 }
