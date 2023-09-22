8e1f04b0eb32664c1fc948165d1e9188207e8c33
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-22 15:44:38 +0200

    add

---
+++ EjerciciosTemaDos/EjerCinco.java
@@ -0,0 +1,11 @@
+public class EjerCinco {
+	public static void main(String[]args){
+	int alto = 15;
+	int ancho = 25;
+	double perimetro = 2*(ancho+alto);
+	double area = alto*ancho;
+	
+	System.out.println("El Perímetro del rectangulo es: " + perimetro);
+	System.out.println("El área del rectangulo es: " + area);
+	}
+}
