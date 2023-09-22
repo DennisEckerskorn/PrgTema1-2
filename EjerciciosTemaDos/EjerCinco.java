c9894eae2cf5c66294a86c049035bfc1d007b29b
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-22 17:03:29 +0200

    Update and add

--- EjerciciosTemaDos/EjerCinco.java
+++ EjerciciosTemaDos/EjerCinco.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
 public class EjerCinco {
 	public static void main(String[]args){
 	//Declaración de las variables
-	int alto = 15;
-	int ancho = 25;
-	//Se calculan las formulas dadas y se guarda el resultado en la variable.
-	int perimetro = 2*(ancho+alto);
-	int area = alto*ancho;
+	Scanner lector = new Scanner(System.in);
+	double alto = 0;
+	double ancho = 0;
 	
+	System.out.println("Dime el ancho del rectangulo");
+	ancho = Double.parseDouble(lector.nextLine());
+
+	System.out.println("Dime el alto del rectangulo");
+	alto = Double.parseDouble(lector.nextLine());
+
+	lector.close();
+
+	//Se calculan las formulas dadas y se guarda el resultado en la variable.
+	double perimetro = ancho * 2 + alto * 2;
+	double area = alto*ancho;
+
 	//Se muestran los resultados por pantalla.
 	System.out.println("El valor dado para el alto del rectangulo son " + alto + "cm " + " y el ancho del rectangulo son " + ancho + "cm "); 
-	System.out.println("El Perímetro del rectangulo es: " + perimetro + "cm");
-	System.out.println("El área del rectangulo es: " + area + "cm");
+	System.out.println("El perímetro del rectangulo son: " + perimetro + "cm");
+	System.out.println("El área del rectangulo son: " + area + "cm");
 	}
 }
---
+++ EjerciciosTemaDos/EjerSeis.java
@@ -0,0 +1,8 @@
+public class EjerSeis {
+	public static void main(String[] args){
+		System.out.println("a) 25>20 && 13>5 " + (25>20 && 13>5));
+		System.out.println("b) 10+4<15-3 || 2*5+1>14-2*2 " + (10+4<15-3 || 2*5+1>14-2*2));
+		System.out.println("c) 4*2<=8 || 2*2<5 && 4 > 3+1 " + (4*2<=8 || 2*2<5 && 4 > 3+1));
+		System.out.println("d) 10<=2*5 && 3<4 || !(8>7) && 3*2<=4*2-1 " + (10<=2*5 &&  3<4 || !(8>7) && 3*2<=4*2-1));
+	}
+}
