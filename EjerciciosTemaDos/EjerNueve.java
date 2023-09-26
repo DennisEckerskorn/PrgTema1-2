661de55768c4cdb941969bbcfe46f18330093e1e
DennisEckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-26 17:06:36 +0200

    add

---
+++ EjerciciosTemaDos/EjerNueve.java
@@ -0,0 +1,11 @@
+public class EjerNueve {
+	public enum Color{
+		RED, GREEN, BLUE
+	}
+	public static void main(String[] args){
+		Color color;
+		color = Color.RED;
+		
+		System.out.println(color);
+	}
+}
