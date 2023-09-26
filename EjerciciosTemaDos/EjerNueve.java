15e0aea771544551e4d611e489ebacef137f474b
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-26 18:02:34 +0200

    upd

--- EjerciciosTemaDos/EjerNueve.java
+++ EjerciciosTemaDos/EjerNueve.java
@@ -1,11 +1,21 @@
 public class EjerNueve {
+	//Declaramos la variable de tipo de referencia enum con varios colores.
 	public enum Color{
 		RED, GREEN, BLUE
 	}
 	public static void main(String[] args){
+		//Declaramos la variable color de tipo Color(enum).
 		Color color;
+
+		//Asignamos y elegimos el valor del enum a la variable color.
 		color = Color.RED;
+		//Mostramos el valor asignado a la variable en la pantalla.
+		System.out.println(color);
 		
+		color = Color.GREEN;
+		System.out.println(color);
+
+		color = Color.BLUE;
 		System.out.println(color);
 	}
 }
