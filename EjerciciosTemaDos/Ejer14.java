8888d9607f6f4d9ac79bfeb5dfc1b69d56de24d9
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-28 23:37:46 +0200

    added 15

--- EjerciciosTemaDos/Ejer14.java
+++ EjerciciosTemaDos/Ejer14.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 public class Ejer14 {
 	public static void main(String[] args) {
 		//Declaración de las variables.
+		DecimalFormat df = new DecimalFormat("0.##");
 		Scanner lector = new Scanner(System.in);
 		double radio, longitud, area;
-		final double PI = 3.14159;
+		final double PI = 3.1416;
 
 		//Pedimos el radio de la circunferencia por pantalla.
 		System.out.println("Dime el Radio de la circunferencia en centímetros");
@@ -15,8 +17,8 @@ public class Ejer14 {
 		area = PI*(radio*radio);
 
 		//Imprimimos los resultados por pantalla.
-		System.out.println("La longitud del radio es " + longitud + " cm.");
-		System.out.println("El área de la circunferencia es " + area + " cm.");
+		System.out.println("La longitud del radio son " + df.format(longitud) + "cm.");
+		System.out.println("El área de la circunferencia son " + df.format(area) + "cm.");
 		//Cerramos el Scanner.
 		lector.close();
 	}
---
+++ EjerciciosTemaDos/Ejer15.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ejer15 {
+	public static void main(String[] args) {
+		//Variables:
+		Scanner lector = new Scanner(System.in);
+		double gradosKelvin;
+		double gradosCelsius;
+		double gradosFahrenheit;
+		//Se pide el valor en grados centígrados al usuario y se guarda el numero en la variable.
+		System.out.println("Ingresa la temperatura en grados centígrados:");
+		gradosCelsius = Double.parseDouble(lector.nextLine());
+		//Operaciones para convertir ºC a K y ºF.
+		gradosKelvin = 273.15 + gradosCelsius;
+		gradosFahrenheit = gradosCelsius * (9/5) + 32;
+		//Imprimimos el resultado por pantalla.
+		System.out.println(gradosCelsius + "ºC son " + gradosKelvin + " K y " + gradosFahrenheit + " ºF");
+	}
+}
