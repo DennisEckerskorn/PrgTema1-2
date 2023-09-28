49d6210b0675986772fd66c5e64a8c2cbb90e4c0
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-28 22:47:03 +0200

    Correcion Ejer13

--- EjerciciosTemaDos/Ejer13.java
+++ EjerciciosTemaDos/Ejer13.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ejer13 {
 	public static void main(String[] args) {
-		//Declaramos las variables del programa, en este caso Long porque segundos pordrian superar el limite de un Entero.
+		//Declaramos las variables del programa.
 		Scanner lector = new Scanner(System.in);
 		int segundos, segundosUsuario, minutos, horas, dias;
 
@@ -10,28 +10,21 @@ public class Ejer13 {
 		System.out.println("Ingresa una cantidad de segundos");
 		segundos = Integer.parseInt(lector.nextLine());
 		segundosUsuario = segundos;
+
 		//Operaciones para convertir los segundos a minutos, horas y dias.
 		minutos = segundos / 60;
 		segundos = segundos % 60;
-		//Los segundos estan ya estan calculado correctamente.
+		//Los segundos ya estan calculado correctamente.
 		horas = minutos / 60;
 		minutos = minutos % 60;
-		//Los minutos estan ya estan calculado correctamente.
+		//Los minutos ya estan calculado correctamente.
 		dias = horas / 24;
 		horas = horas % 24;
-		//Las horas estan ya estan calculado correctamente.
+		//Las horas ya estan calculado correctamente.
 		//El resto que queda son dias.
 
-		//Sacamos los resultados por pantalla, si el resultado es 1 segundo, 1 minuto o 1 hora se saca un mensaje adecuado por pantalla.
-		if (minutos == 1){
-			System.out.println(segundosUsuario + " segundos es " + minutos + " minuto " + horas + " horas y " + dias + " dias.");
-		} else if (horas == 1){
-			System.out.println(segundosUsuario + " segundos son " + minutos + " minutos " + horas + " hora y " + dias + " dias.");
-		}else if (dias == 1){
-			System.out.println(segundosUsuario + " segundos son " + minutos + " minutos " + horas + " horas y " + dias + " dia.");
-		}else{
-			System.out.println(segundosUsuario + " segundos son " + minutos + " minutos " + horas + " horas y " + dias + " dias.");
-		}
+		//Sacamos los resultados por pantalla, mostrando los segundos ingresados y los resultados calculados.
+		System.out.println(segundosUsuario + " segundos son " + dias + " días, " + horas + " horas, " + minutos + " minutos, " + segundos + " segundos.");
 		//Cerramos el Scanner.
 		lector.close();		
 	}
