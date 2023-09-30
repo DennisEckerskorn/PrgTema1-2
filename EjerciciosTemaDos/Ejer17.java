ac4c74e67baf117d528cf06b9bee5a14b1a1f7b3
Dennis Eckerskorn <45369024+DennisEckerskorn@users.noreply.github.com>
2023-09-30 08:54:40 +0200

    Update Ejer17

--- EjerciciosTemaDos/Ejer17.java
+++ EjerciciosTemaDos/Ejer17.java
@@ -5,51 +5,52 @@ public class Ejer17 {
 		Scanner lector = new Scanner(System.in);
 		double precioLitro;
 		double totalPagado;
+		double totalPagadoPrimera;
+		double totalPagadoSegunda;
 		double kmCuentakilometrosPrincipio;
 		double kmCuentakilometrosFinal;
-		double litrosRepostadosPrimer;
+		double litrosRepostadosPrimera;
 		double litrosRepostadosSegunda;
 		double kmRecorridos;
 		double consumoMedio;
 		double totalLitros;
 		double costeKilometro;
 
-		//Se piden los datos del primer repostaje
+		//Se piden los datos del primer repostaje y se guardan los valores en las variables correspondientes.
 		System.out.println("¿Cuánto cuesta el litro del combustible en el primer repostaje?");
 		precioLitro = Double.parseDouble(lector.nextLine());
-
 		System.out.println("¿Cuánto has pagado por el primer repostaje?");
-		totalPagado = Double.parseDouble(lector.nextLine());
-		
+		totalPagadoPrimera = Double.parseDouble(lector.nextLine());
 		System.out.println("¿Cuántos KM te marca el cuentakilómetros");
 		kmCuentakilometrosPrincipio = Double.parseDouble(lector.nextLine());
+
 		//Se sacan los litros repostados del primer repostaje.
-		litrosRepostadosPrimer = totalPagado / precioLitro;
+		litrosRepostadosPrimera = totalPagadoPrimera / precioLitro;
 		//Se saca el resultado por pantalla.
-		System.out.println("Has repostado " + litrosRepostadosPrimer + " litros en el primer repostaje.");
-		//Se piden los datos del segundo repostaje.
+		System.out.println("Has repostado " + litrosRepostadosPrimera + " litros en el primer repostaje.");
+
+		//Se piden los datos del segundo repostaje y se guarda en las variables correspondientes.
 		System.out.println("¿Cuánto cuesta el litro de combustible en el segundo repostaje?");
 		precioLitro = Double.parseDouble(lector.nextLine());
-		
 		System.out.println("¿Cuánto has pagado por el segundo repostaje");
-		totalPagado = Double.parseDouble(lector.nextLine());
+		totalPagadoSegunda = Double.parseDouble(lector.nextLine());
 
-		litrosRepostadosSegunda = totalPagado / precioLitro;
-
+		//Se sacan los litros repostados del segundo repostaje.
+		litrosRepostadosSegunda = totalPagadoSegunda / precioLitro;
+		//Se saca el resultado por pantalla.
 		System.out.println("Has repostado " + litrosRepostadosSegunda + " litros en el segundo repostaje.");
-
 		System.out.println("¿Cuántos KM te marca el cuentakilómetros");
 		kmCuentakilometrosFinal = Double.parseDouble(lector.nextLine());
 
+		//Operaciones para sacar los valores que nos hacen falta.
 		kmRecorridos = kmCuentakilometrosFinal - kmCuentakilometrosPrincipio;
-
-		System.out.println(kmRecorridos);
-		
-		totalLitros = litrosRepostadosPrimer + litrosRepostadosSegunda;
+		totalLitros = litrosRepostadosPrimera + litrosRepostadosSegunda;
 		consumoMedio = totalLitros * 100 / kmRecorridos;
-		costeKilometro = totalPagado / kmRecorridos; //falta sumar totalpagado
-
-		System.out.println("El consumo medio cada 100KM son " + consumoMedio + " litros.");
+		totalPagado = totalPagadoPrimera + totalPagadoSegunda;
+		costeKilometro = totalPagado / kmRecorridos;
+		
+		System.out.println("Has recorrido " + kmRecorridos + " KM, desde tu primer repostaje");
+		System.out.println("El consumo medio cada 100 KM son " + consumoMedio + " litros.");
 		System.out.println("El coste para cada kilometro son " + costeKilometro + " euros.");
 	}
 }
